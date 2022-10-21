3958537c148a59abd6ec82b341be47bdcc79fead
Anastasia <nastya-shishkina-1999@mail.ru>
2022-10-21 21:04:46 +0300

    task 3

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+Console.WriteLine("Программа, которая проверяет, является ли выбранный Вами день выходным");
+Console.WriteLine("Введите номер дня недели: ");
+string[]days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+int NumberOfDay = Convert.ToInt32(Console.ReadLine());
+while(NumberOfDay < 6)
+{
+    Console.WriteLine(days[NumberOfDay - 1]);
+    Console.WriteLine("Это будний день(");
+    break;
+}
+    while(NumberOfDay >= 6)
+    {
+    Console.WriteLine(days[NumberOfDay - 1]);
+    Console.WriteLine("Это выходной!)");
+    break;
+    }
